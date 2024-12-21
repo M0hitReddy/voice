3c10c4c919168a199053e7d1998e6a0a6c71c369
M0hitReddy <mohitreddydadireddy@gmail.com>
2024-12-22 01:14:48 +0530

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-import { Nav } from "@/components/Nav";
+
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
@@ -24,7 +24,7 @@ export default function RootLayout({
           "flex flex-col min-h-screen"
         )}
       >
-        <Nav />
+
         {children}
       </body>
     </html>
