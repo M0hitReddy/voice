4a099b39e4495366972f08e122e28757291ee697
M0hitReddy <mohitreddydadireddy@gmail.com>
2024-12-22 00:41:00 +0530

    initial done

---
+++ components/Audio.tsx
@@ -0,0 +1,23 @@
+// "use client"
+// import {
+//   type CloseHandler,
+//   EmbeddedVoice as EA,
+  
+//   type EmbeddedVoiceConfig,
+//   type TranscriptMessageHandler,
+// } from "@humeai/voice-embed";
+
+// export default function Audio() {
+//   const embeddedVoice = EA.create({
+//     auth: { type: "apiKey", value: process.env.HUMEAI_API_KEY || "" },
+//     configId: process.env.NEXT_PUBLIC_HUME_CONFIG_ID || "",
+//     rendererUrl: "http://localhost:3000",
+//   });
+//   console.log("embeddedVoice", embeddedVoice);
+//   return (
+//   <>
+  
+//   {/* <div id="hume-embedded-voice" />; */}
+//   </>
+//   )
+// }
